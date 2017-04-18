452429a83fbe33aee603883b1d93c5bb9faefefd
maiduo <iamdbd@163.com>
2017-04-18 16:04:04 +0800

    vue demo todo list second commit

--- vue-todo/app.js
+++ vue-todo/app.js
@@ -13,26 +13,53 @@ new Vue({
 	el:".main",
 	data:{
 		list:list,
-		todo:""
+		todo:"",
+		edit:"",    //记录正在编辑的数据
+		beforeTitle:""
+	},
+	computed:{
+		noChecklength:function(){
+			return this.list.filter(function(item){
+				return !item.isChecked;
+			}).length
+		}
 	},
 	methods:{
 		/*addTodo:function() {
 			// add task
 
 		}*/
-		addTodo(ev){
-			//es6
-			// add task
+		addTodo(ev){// add task
 			this.list.push({
 				title:this.todo,
 				isChecked:false
 			});
 			this.todo = "";
 		},
-		deleteTodo(todo){
-			//delete task
+		deleteTodo(todo){//delete task
 			var index = this.list.indexOf(todo);
 			this.list.splice(index,1);
+		},
+		editTodo(todo){//edit task
+			this.beforeTitle = todo.title
+			this.edit = todo;
+		},
+		editTodoed(todo){ //编辑任务成功
+			this.edit = "";
+		},
+		cancelTodo(todo){//取消编辑任务
+			todo.title = this.beforeTitle;
+			this.edit = "";
+		}
+
+	},
+	directives:{
+		focus:{
+			update(el,binding){
+				if (binding.value) {
+					el.focus();
+				}
+			}
 		}
 	}
 });
